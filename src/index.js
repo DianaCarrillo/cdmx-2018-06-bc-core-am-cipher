5a664b8378a75cb5cd3ac8c88fb7058d150a8c4a
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-06-18 09:41:46 -0500

    cambios en el readme y la interface

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 let string1=document.getElementById("input");// EL VALOR QUE SE INTRODUCE EN EL INPUT
 let offset1=document.getElementById("offset"); // obtener offset y convirtiendo de string a number
+let botonCifrar=document.getElementById("encodeClick");
+let botonDefifrar=document.getElementById("decodeClick");
+let resultado=document.getElementById("sms");
+let parrafoResultado=document.getElementById("cifradoDescifrado");
+let botonReset=document.getElementById("cifradoDescifrado");
 
+// ocultar reset y resultado antes de la función
+resultado.style.display="none";
+parrafoResultado.style.display="none";
+botonReset.style.display="none";
 
-
-
+// llamando a l click para que ejecute la función encode.
   document.getElementById("encodeClick").addEventListener("click", obtenerDatos=()=>{
     let string=string1.value;
       let offset=parseInt(offset1.value);
-      cipher.encode(offset,string);
-    document.getElementById("sms").value=cipher.encode(offset,string);
+      // cipher.encode(offset,string);
+      resultado.value=cipher.encode(offset,string);
 
+  // ocultar después del click
+      string1.style.display="none";
+      offset1.style.display="none";
+      botonCifrar.style.display="none";
+      botonDefifrar.style.display="none";
+    // mostrar después del click
+      resultado.style.display="block";
+      parrafoResultado.style.display="block";
+      botonReset.style.display="block";
   });
 
 
@@ -17,5 +34,15 @@ let offset1=document.getElementById("offset"); // obtener offset y convirtiendo 
       let string=string1.value;
         let offset=parseInt(offset1.value);
         cipher.decode(offset,string);
-    document.getElementById("sms").value=cipher.decode(offset,string);
+        resultado.value=cipher.decode(offset,string);
+
+  // ocultar después del click
+      string1.style.display="none";
+      offset1.style.display="none";
+      botonCifrar.style.display="none";
+      botonDefifrar.style.display="none";
+      // mostrar después del click
+      botonReset.style.display="block";
+      parrafoResultado.style.display="block";
+      resultado.style.display="block";
       });
